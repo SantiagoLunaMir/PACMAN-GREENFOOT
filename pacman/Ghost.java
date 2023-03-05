bebcc2185b3d433840eeedc0dfd89dc4b5b985b7
Santiago Luna Mireles <111355326+SantiagoLunaMir@users.noreply.github.com>
2023-03-05 13:17:18 -0600

    PACMAN commit

---
+++ pacman/Ghost.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ghost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ghost extends Item
+{
+    public Ghost(){
+        super(-15);
+    }
+    /**
+     * Act - do whatever the Ghost wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
